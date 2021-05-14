9d5b7486a514143909d31c33b57faf825c96cfd6
t4t5u0 <ymmtryk0902@gmail.com>
2021-05-14 22:02:52 +0900

    [add] 未提出のタスクを取得できた

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import requests as rq
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, element
 
-USERID = ''
-PASSWORD = ''
+USERID = 'id'
+PASSWORD = 'pass'
 
 base_url = 'https://manaba.fun.ac.jp/ct/'
 url = base_url + 'login'
@@ -15,15 +15,28 @@ session = rq.session()
 session.get(url)
 
 login = session.post(url, data=login_data)
-# print(login.text)
 
 bs = BeautifulSoup(login.text, 'lxml')
-# print(bs)
 courses = bs.find_all('td', class_='course')
-print(len(courses))
 for course in courses:
-    if course.a:
-        print(course.a.get('href'), course.a.text)
-    # break
-        c = session.get(base_url + course.a.get('href'))
-        print(c)
+    if course.find('a'):
+        report_url = base_url + course.find('a').get('href') + '_report'
+        report = BeautifulSoup(session.get(report_url).text, 'lxml')
+        course_name = report.find('a', id='coursename').get_text()
+        if course.find('a').get('href') == 'course_96360':  # テストのため決め打ち
+            print(course_name)
+            table = report.find_all('table', class_='stdlist')
+            for row in table:
+                row: element.Tag
+                tasks: list[element.Tag] = row.find_all('tr')[1:]  # １つ目の課題
+                for task in tasks:
+                    title: element.Tag
+                    state: element.Tag
+                    start: element.Tag
+                    end: element.Tag
+                    title, state, start, end = task.find_all('td')
+                    if state.find('div').get_text() == '受付中' and state.find('span', class_='deadline').get_text() == '未提出':
+                        print(title.find('a').get_text())
+                        print(state.find('span', class_='deadline').get_text())
+                        print(start.get_text())
+                        print(end.get_text())
